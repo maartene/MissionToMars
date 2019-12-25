ea2f606e8fcb7759db346834a1b6823275954858
Maarten Engels <maartene@mac.com>
2019-12-25 11:54:27 +0100

    explicit types for static arrays Stage Component

--- Sources/Model/Component.swift
+++ Sources/Model/Component.swift
@@ -37,7 +37,7 @@ public struct Component: Equatable, Codable {
         case componentAlreadyBeingBuilt
     }
     
-    public static let allComponents = [
+    public static let allComponents: [Component] = [
         Component(shortName: .Rocket_S, name: "Small reusable rocket", description: "Although it is technically possible to reach Mars without reusable rockets, considering the large number of flights required the cost would be prohibitive. Although this first reusable rocket will be more expensive than a single use rocket, the subsequent ones will be much MUCH cheaper.", cost: 2_000_000_000, buildTime: 365),
         Component(shortName: .MissionControl, name: "Mission Control", description: "All your missions will be guided from here. You only need to build one once, these are the initial costs. It will be upgraded as per technological advancements. It will also receive upgrades when building subsequent components.", cost: 1_000_000_000, buildTime: 365 / 2),
         Component(shortName: .Satellite, name: "Mars Orbital Satellite", description: "This sattelite has three major purposes: \n1) Provide imagery of Mars surface to help find suitable spot for colony \n2) Provide local communation \n3) Provide a beach head for communication with surface deployments", cost: 1_000_000_000, buildTime: 3 * 365 / 4 ),
